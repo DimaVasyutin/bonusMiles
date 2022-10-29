d11a068a7b66bb109669865021ddb1cdfe0f554d
Dima Vasyutin <Vasyutin_dima12@mail.ru>
2022-10-29 19:46:55 +0300

    Add programm bonusMiles

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        float priceTiket = 14552.56F;
+        byte oneMileCost =20;
+        int sumMiles = (int)priceTiket / oneMileCost;
+        System.out.println(sumMiles);
+    }
+}
