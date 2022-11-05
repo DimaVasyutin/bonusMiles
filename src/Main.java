0ff268704b603e85603d94ed49ffcfc9993cb1cc
DimaVasyutin <115107603+DimaVasyutin@users.noreply.github.com>
2022-11-05 13:18:51 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         float priceTiket = 14552.56F;
-        byte oneMileCost =20;
-        int sumMiles = (int)priceTiket / oneMileCost;
+        byte oneMileCost = 20;
+        int sumMiles = (int) priceTiket / oneMileCost;
         System.out.println(sumMiles);
     }
 }
